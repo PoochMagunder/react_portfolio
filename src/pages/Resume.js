7136433531833fe7397176c759e0137c7574e1e9
DevonEadie <116775183+PoochMagunder@users.noreply.github.com>
2023-06-01 11:15:19 -0700

    updated package.json

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,11 +4,11 @@ import ResumeImg from '../assets/img/Screenshot (1).png';
 
 const Resume = () => {
 
-    const downloadTxtFile = () => {
-        const downloadlink =  document.createElement("a");
-        element.href = URL.createObjectURL("file:///S:/Documents/PDF's/Resume/Devon_Eadie_-_.pdf");
-        element.download = "100ideas-" + Date.now() + ".txt";
-    }
+    // // const downloadTxtFile = () => {
+    // //     const downloadlink =  document.createElement("a");
+    // //     element.href = URL.createObjectURL("file:///S:/Documents/PDF's/Resume/Devon_Eadie_-_.pdf");
+    // //     element.download = "100ideas-" + Date.now() + ".txt";
+    // }
 
     return (
         <div className="resume-wrapper">
@@ -20,7 +20,7 @@ const Resume = () => {
                         src={ResumeImg}
                         alt="img-of-resume">
                 </img>
-               <button id="downloadBtn" onClick={downloadTxtFile} value="download">Download Resume</button>
+               <button id="downloadBtn" value="download">Download Resume</button>
             </div>
         </div>
     )
