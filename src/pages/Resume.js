f846595e3ffe26a3bf996a77f09b5d5d0c4ba772
DevonEadie <116775183+PoochMagunder@users.noreply.github.com>
2023-07-14 15:55:31 -0700

    added some package.json code

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,12 +4,6 @@ import ResumeImg from '../assets/img/Screenshot (1).png';
 
 const Resume = () => {
 
-    // // const downloadTxtFile = () => {
-    // //     const downloadlink =  document.createElement("a");
-    // //     element.href = URL.createObjectURL("file:///S:/Documents/PDF's/Resume/Devon_Eadie_-_.pdf");
-    // //     element.download = "100ideas-" + Date.now() + ".txt";
-    // }
-
     return (
         <div className="resume-wrapper">
             <header className="resume-header">
