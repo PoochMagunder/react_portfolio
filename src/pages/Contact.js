39f93d0da4efb4a182d2ccb28d0ddd5a0750c59a
DevonEadie <116775183+PoochMagunder@users.noreply.github.com>
2023-07-20 11:42:30 -0700

    added button function to hide contact info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,19 +2,33 @@ import React from "react";
 import '../assets/css/Contact.css';
 
 const Contact = () => {
-
-
+    const [showContact, setShowContact] = React.useState(false)
+    const onClick = () => setShowContact(true)
     return (
         <div className="contact-wrapper">
-            <header className="contact-header">
-                Contact Info
-            </header>
+            <div className="contact-header">
+                <header>Contact Me</header>
+            </div>
+
             <div className="contact-container">
-                <p id="contact">Phone Number: 1-250-218-6093</p>
-                <p id="contact">Email: Devon.eadie@yahoo.com</p>
+                <div className="inner-container">
+                    <p>
+                        Disclaimer:
+
+                        Please be advised that I do not wish to receive any unsolicited emails, spam, or pointless messages. Any communication directed to my email address should be relevant, meaningful, and in line with the purpose of our interaction. Any misuse or violation of this request may result in the sender being blocked or reported as appropriate. Thank you for respecting my preferences.</p>
+                    <input type="submit" className="contact-btn" value="Contact Info" onClick={onClick} />
+                    {showContact ? <Results /> : null}
+                </div>
             </div>
         </div>
     )
-};
+}
+
+const Results = () => (
+    <div id="results" className="contact">
+        Email : Devon.eadie@yahoo.com
+    </div>
+)
 
 export default Contact;
+
