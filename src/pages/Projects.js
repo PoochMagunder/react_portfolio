8abde27ee42a5a0cedc91ae705df53927fce7ee2
DevonEadie <116775183+PoochMagunder@users.noreply.github.com>
2023-10-13 11:28:40 -0700

    linked new website

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 import '../assets/css/Projects.css'
 import FirstP from '../assets/img/first-project.jpg';
 import SecondP from '../assets/img/Valorant_Cover_Art.webp';
-import ThirdP from '../assets/img/mangaverse.jpg';
+import ThirdP from '../assets/img/logo.png';
 
 const Projects = () => {
 
@@ -17,15 +17,15 @@ const Projects = () => {
             <div className="project-container">
             <Card as={Link} to={'https://poochmagunder.github.io/Portfolio-Website/'} className="first-project-card" bg="dark" style={{ width: '18rem'}}>
                 <Card.Header style={{ color: "white"}}>My First Webpage</Card.Header>
-                <Card.Img variant="top" src={FirstP} />
+                <Card.Img className="cardimgs" variant="top" src={FirstP} />
             </Card>
             <Card as={Link} to={'https://project-friendly-fire.herokuapp.com/'} className="second-project-card" bg="dark" style={{ width: '18rem'}}>
                 <Card.Header style={{ color: "white"}}>Friendly Fire</Card.Header>
-                <Card.Img variant="top" src={SecondP} />
+                <Card.Img className="cardimgs" variant="top" src={SecondP} />
             </Card>
-            <Card as={Link} to={'https://manga-verse.herokuapp.com/'} className="third-project-card" bg="dark" style={{ width: '18rem'}}>
-                <Card.Header style={{ color: "white"}}>MangaVerse</Card.Header>
-                <Card.Img variant="top" src={ThirdP} />
+            <Card as={Link} to={'https://ling-king-fishing-a75c68586413.herokuapp.com'} className="third-project-card" bg="dark" style={{ width: '18rem'}}>
+                <Card.Header style={{ color: "white"}}>Ling King Fishing</Card.Header>
+                <Card.Img className="cardimgs" variant="top" src={ThirdP} />
             </Card>
             </div>
         </div>
