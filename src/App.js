aea0da8264d5290e2fd4e547184adfa43ee70057
DevonEadie <116775183+PoochMagunder@users.noreply.github.com>
2023-12-01 12:26:05 -0800

    added fourth project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/Navigation';
-import Footer from './components/Footer'
-import About from './pages/Aboutme';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Navigation";
+import Footer from "./components/Footer";
+import About from "./pages/Aboutme";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
     <Router>
       <Nav />
       <Routes>
-        <Route
-          path='/'
-          element={<About />}
-        />
-        <Route
-          path='/projects'
-          element={<Projects />}
-        />
-        <Route
-          path='/contact'
-          element={<Contact />}
-        />
-        <Route
-          path='/resume'
-          element={<Resume />}
-          />
+        <Route path="/" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
       </Routes>
       <Footer />
     </Router>
